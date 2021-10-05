7d02ba120bc3a18064852857098a7465fb36b485
rosen.hayden <rosen.hayden@vashonsd.org>
2021-10-05 10:46:50 -0700

    Initial commit

---
+++ src/org/vashon/Main.java
@@ -0,0 +1,12 @@
+package org.vashon;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println(squareOf(4));
+    }
+
+    public static int squareOf(int a){
+        return a * a;
+    }
+}
